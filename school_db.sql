d9a21cd20b1feb0a81b00f616e32e030c5c3f524
Mamadou Siradiou Diallo <mamadousiradioudiallo4@gmail.com>
2024-10-14 18:11:36 +0200

    Creation of Professeur table

--- school_db.sql
+++ school_db.sql
@@ -1,9 +1,18 @@
 CREATE TABLE Etudiant (
-    id_etudiant  INT AUTO_INCREMENT PRIMARY KEY,
+    id_etudiant INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(40),
     prenom VARCHAR(50),
     date_naissance DATE,
     adresse VARCHAR(255),
     email VARCHAR(100) UNIQUE,
     telephone VARCHAR(15)
+);
+
+CREATE TABLE Professeur (
+    id_professeur INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(40),
+    prenom VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    telephone VARCHAR(15),
+    departement VARCHAR(100)
 );
